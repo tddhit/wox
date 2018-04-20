b082ab7597288699b3ba64285d9f55b510e7bb83
tangdandang@meizu.com <tangdandang@meizu.com>
2018-04-20 17:24:05 +0800

    增加GetInternalAddr/GetExternalAddr接口

--- naming/registry.go
+++ naming/registry.go
@@ -60,6 +60,42 @@ func (r *Registry) Close() {
 }
 
 func GetLocalAddr(listenAddr string) string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if isExternalIP(ipnet.IP) {
+				continue
+			} else {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
+
+func isExternalIP(IP net.IP) bool {
+	if IP.IsLoopback() || IP.IsLinkLocalMulticast() || IP.IsLinkLocalUnicast() {
+		return false
+	}
+	if ip4 := IP.To4(); ip4 != nil {
+		switch true {
+		case ip4[0] == 10:
+			return false
+		case ip4[0] == 172 && ip4[1] >= 16 && ip4[1] <= 31:
+			return false
+		case ip4[0] == 192 && ip4[1] == 168:
+			return false
+		default:
+			return true
+		}
+	}
+	return false
+}
+
+func GetExternalAddr(listenAddr string) string {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
 		log.Fatal(err)
